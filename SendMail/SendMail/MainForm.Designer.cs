b47fe2b8c0ae29cc810693d4c5502644c2cc38a7
iilie <iilie@axway.com>
2022-03-22 10:34:44 +0200

    INT - Adding SendMail demo.

---
+++ SendMail/SendMail/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SendMail
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.Label label1;
+			System.Windows.Forms.Label label2;
+			System.Windows.Forms.Label label3;
+			System.Windows.Forms.Label label4;
+			System.Windows.Forms.Button button;
+			this.textBoxFrom = new System.Windows.Forms.TextBox();
+			this.textBoxTo = new System.Windows.Forms.TextBox();
+			this.textBoxSubject = new System.Windows.Forms.TextBox();
+			this.textBoxBody = new System.Windows.Forms.TextBox();
+			label1 = new System.Windows.Forms.Label();
+			label2 = new System.Windows.Forms.Label();
+			label3 = new System.Windows.Forms.Label();
+			label4 = new System.Windows.Forms.Label();
+			button = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.Location = new System.Drawing.Point(13, 13);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(55, 23);
+			label1.TabIndex = 0;
+			label1.Text = "From";
+			label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBoxFrom
+			// 
+			this.textBoxFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxFrom.Location = new System.Drawing.Point(74, 15);
+			this.textBoxFrom.Name = "textBoxFrom";
+			this.textBoxFrom.Size = new System.Drawing.Size(714, 20);
+			this.textBoxFrom.TabIndex = 1;
+			// 
+			// textBoxTo
+			// 
+			this.textBoxTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxTo.Location = new System.Drawing.Point(74, 41);
+			this.textBoxTo.Name = "textBoxTo";
+			this.textBoxTo.Size = new System.Drawing.Size(714, 20);
+			this.textBoxTo.TabIndex = 3;
+			// 
+			// label2
+			// 
+			label2.Location = new System.Drawing.Point(13, 39);
+			label2.Name = "label2";
+			label2.Size = new System.Drawing.Size(55, 23);
+			label2.TabIndex = 2;
+			label2.Text = "To";
+			label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBoxSubject
+			// 
+			this.textBoxSubject.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSubject.Location = new System.Drawing.Point(74, 67);
+			this.textBoxSubject.Name = "textBoxSubject";
+			this.textBoxSubject.Size = new System.Drawing.Size(714, 20);
+			this.textBoxSubject.TabIndex = 5;
+			// 
+			// label3
+			// 
+			label3.Location = new System.Drawing.Point(13, 65);
+			label3.Name = "label3";
+			label3.Size = new System.Drawing.Size(55, 23);
+			label3.TabIndex = 4;
+			label3.Text = "Subject";
+			label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label4
+			// 
+			label4.Location = new System.Drawing.Point(13, 90);
+			label4.Name = "label4";
+			label4.Size = new System.Drawing.Size(55, 23);
+			label4.TabIndex = 6;
+			label4.Text = "Body";
+			label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBoxBody
+			// 
+			this.textBoxBody.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxBody.Location = new System.Drawing.Point(74, 93);
+			this.textBoxBody.Multiline = true;
+			this.textBoxBody.Name = "textBoxBody";
+			this.textBoxBody.Size = new System.Drawing.Size(714, 345);
+			this.textBoxBody.TabIndex = 7;
+			// 
+			// button
+			// 
+			button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			button.Location = new System.Drawing.Point(13, 415);
+			button.Name = "button";
+			button.Size = new System.Drawing.Size(55, 23);
+			button.TabIndex = 8;
+			button.Text = "Send";
+			button.UseVisualStyleBackColor = true;
+			button.Click += new System.EventHandler(this.button_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(button);
+			this.Controls.Add(this.textBoxBody);
+			this.Controls.Add(label4);
+			this.Controls.Add(this.textBoxSubject);
+			this.Controls.Add(label3);
+			this.Controls.Add(this.textBoxTo);
+			this.Controls.Add(label2);
+			this.Controls.Add(this.textBoxFrom);
+			this.Controls.Add(label1);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Send Mail";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxFrom;
+		private System.Windows.Forms.TextBox textBoxTo;
+		private System.Windows.Forms.TextBox textBoxSubject;
+		private System.Windows.Forms.TextBox textBoxBody;
+	}
+}
+
---
+++ SendMail/SendMail/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace SendMail
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (textBoxFrom.Text.Trim().Length > 0
+					&& textBoxTo.Text.Trim().Length > 0
+					&& textBoxSubject.Text.Length > 0
+					&& textBoxBody.Text.Trim().Length > 0)
+				{
+					var message = new MailMessage {
+						From = new MailAddress(textBoxFrom.Text.Trim()),
+						Subject = textBoxSubject.Text.Trim(),
+						Body = textBoxBody.Text.Trim(),
+						IsBodyHtml = true
+					};
+					foreach (var to in textBoxTo.Text.Trim().Split(','))
+					{
+						message.To.Add(new MailAddress(to.Trim()));
+					}
+					using (var client = new SmtpClient {
+						Host = Settings.Default.Host,
+						Port = Settings.Default.Port,
+						EnableSsl = false,
+						DeliveryMethod = SmtpDeliveryMethod.Network,
+						Credentials = new NetworkCredential {
+							UserName = "admin@home.ro", Password = "admin"}
+					})
+					{
+						client.SendCompleted += (target, arguments) =>
+						{
+							if (arguments.Error == null)
+							{
+								if (arguments.Cancelled)
+								{
+									MessageBox.Show(this, "Message was cancled.", "Warning",
+										MessageBoxButtons.OK, MessageBoxIcon.Warning);
+								}
+								else
+								{
+									MessageBox.Show(this, "Message was sent.", "Information",
+									MessageBoxButtons.OK, MessageBoxIcon.Information);
+								}
+								
+							}
+							else
+							{
+								MessageBox.Show(this, arguments.Error.Message, "Error",
+									MessageBoxButtons.OK, MessageBoxIcon.Error);
+							}
+							
+						};
+						client.SendAsync(message, null);
+						
+					}
+				}
+			} catch (Exception exception)
+			{
+				MessageBox.Show(this, exception.Message, "Error",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
