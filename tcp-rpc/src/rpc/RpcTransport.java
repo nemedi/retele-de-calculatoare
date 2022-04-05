e33269a55740a035da5dbb5801c0deb3c7c24bc3
Iulian Ilie-Nemedi <iilie@axway.com>
2022-04-05 08:08:01 +0300

    INT - Adding method to ignore reflection warning.

--- tcp-rpc/src/rpc/RpcTransport.java
+++ tcp-rpc/src/rpc/RpcTransport.java
@@ -1,5 +1,7 @@
 package rpc;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -13,6 +15,28 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
 class RpcTransport {
+	
+	static {
+		disableAccessWarnings();
+	}
+	
+	public static void disableAccessWarnings() {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class<?> loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
+    }
 	
 	private static Gson gson = new GsonBuilder()
 			.registerTypeAdapter(RpcRequest.class, new RpcRequestDeserializer())
